a4fc63ebd5e72be43bb451ef3b85813cd05f4684
irinazar <iramiami@yandex.ru>
2024-01-30 20:19:02 +0300

    h

--- src/main.tsx
+++ src/main.tsx
@@ -2,16 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import { ChakraProvider } from "@chakra-ui/react";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./features/store.ts";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <HashRouter>
+  <BrowserRouter>
     <ChakraProvider>
       <Provider store={store}>
         <App />
       </Provider>
     </ChakraProvider>
-  </HashRouter>
+  </BrowserRouter>
 );
