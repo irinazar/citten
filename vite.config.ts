cf85a315f9eb36f1cae26d2f19a973d12d7bf3bd
irinazar <iramiami@yandex.ru>
2024-01-31 23:36:29 +0300

    final

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/",
+  base: "/[irinazar.github.io]/",
   plugins: [react()],
-  esbuild: {
-    format: "esm",
-  },
 });
