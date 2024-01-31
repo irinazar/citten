b31d3e987b62318bd5da4cdeb027f5b28ba4f18a
irinazar <iramiami@yandex.ru>
2024-01-31 23:49:16 +0300

    Updates

---
+++ assets/FavoriteCats-CbBbyjd2.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-fe0iB_lH.js";import{u as a,s as e,C as o}from"./AllCats.module-Km_o9yNp.js";function m(){const t=a(s=>s.cats.favoriteCats);return r.jsx("div",{className:e.container,children:t==null?void 0:t.map(s=>r.jsx(o,{cat:s},s.id))})}export{m as default};
