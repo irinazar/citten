0c261439bc15e50664242ca3b5ca7f5701142e18
irinazar <iramiami@yandex.ru>
2024-01-31 23:41:20 +0300

    Updates

---
+++ assets/FavoriteCats-iEMUn1-a.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-_BFlhw6e.js";import{u as a,s as e,C as o}from"./AllCats.module-8Fwp3wtc.js";function m(){const t=a(s=>s.cats.favoriteCats);return r.jsx("div",{className:e.container,children:t==null?void 0:t.map(s=>r.jsx(o,{cat:s},s.id))})}export{m as default};
